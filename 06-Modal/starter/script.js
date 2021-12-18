0ee4ac65c0420c4547685a9bd51659783f2252e6
RayDugan <raymond.j.dugan@gmail.com>
2021-12-18 09:41:35 -0600

    worked on modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal').textContent;
 const overlay = document.querySelector('.overylay');
 const btnCloseModal = document.querySelector('close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
+const btnsOpenModal = document.querySelectorAll('.show-modal');
