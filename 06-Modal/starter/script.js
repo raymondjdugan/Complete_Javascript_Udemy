de8a94dbdee5eeada461ed28374fb86f2ffc2419
RayDugan <raymond.j.dugan@gmail.com>
2021-12-19 20:54:55 -0600

    changed variable names

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,4 +5,3 @@ const overlay = document.querySelector('.overylay');
 const btnCloseModal = document.querySelector('close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
-const btnsOpenModal = document.querySelectorAll('.show-modal');
