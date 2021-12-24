6f4b61f82f4f06025f39e0fb27cf7116d5d4e48c
RayDugan <raymond.j.dugan@gmail.com>
2021-12-24 14:53:58 -0600

    Continuation of refactoring code for developer skills. Adding notes for developer skills and behind the scenes. Added notes for data structures

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,67 @@ const restaurant = {
     },
   },
 };
+
+//Object Destructuring
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//This assigns the object methods to different names for the stored variables
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Setting default values for object destructuring
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating varibles in object destructuring
+let a = 111;
+let b = 119;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//Destructuring nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+//Array Destructuring
+//Instead of this
+// const arr = [2, 3, 4];
+// const a = [0];
+// const b = [1];
+// const c = [2];
+
+//We can this
+// const [x, y, z] = arr; //This assigns the indexes to x, y and z as variables
+
+// let [main, secondary] = restaurant.categories; //This will grab the first and second index of the array
+// console.log(main, secondary);
+// // const [first, , third] = restaurant.categories; //This will grab the first and third index of the array
+
+// //Old way of switching values of indexes
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
+
+// //Switches the values in the indexes of an array using destructuring
+// [main, secondary] = [secondary, main];
+
+// //Recieve 2 return values from the order function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+
+// //Nested Destructuring
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// //Default values
+// const [p = 1, q = 1, r = 1] = [8];
+// console.log(p, q, r);
