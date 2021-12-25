d4268234bc14d9c50c9e7414208e5ee3b424a3b9
RayDugan <raymond.j.dugan@gmail.com>
2021-12-24 20:09:17 -0600

    Took notes on lecture of data structures. Did coding challenge 1 on data structures

---
+++ 09-Data-Structures-Operators/starter/codingChallenge1.js
@@ -0,0 +1,69 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(playersFinal);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+function printGoals(...playerNames) {
+  console.log(playerNames);
+  console.log(`Goals scored: ${playerNames.length}`);
+}
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -16,6 +16,18 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
+  //Destructruing in function parameters
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -30,68 +42,211 @@ const restaurant = {
       close: 24,
     },
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicous pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
-//Object Destructuring
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-//This assigns the object methods to different names for the stored variables
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
-
-//Setting default values for object destructuring
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
-
-//Mutating varibles in object destructuring
-let a = 111;
-let b = 119;
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj);
-console.log(a, b);
-
-//Destructuring nested objects
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close);
-//Array Destructuring
-//Instead of this
+// Array Destructuring
+// Instead of this
 // const arr = [2, 3, 4];
 // const a = [0];
 // const b = [1];
 // const c = [2];
 
-//We can this
+// We can this
 // const [x, y, z] = arr; //This assigns the indexes to x, y and z as variables
 
 // let [main, secondary] = restaurant.categories; //This will grab the first and second index of the array
 // console.log(main, secondary);
-// // const [first, , third] = restaurant.categories; //This will grab the first and third index of the array
+// const [first, , third] = restaurant.categories; //This will grab the first and third index of the array
 
-// //Old way of switching values of indexes
-// // const temp = main;
-// // main = secondary;
-// // secondary = temp;
-// // console.log(main, secondary);
+//Old way of switching values of indexes
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
 
-// //Switches the values in the indexes of an array using destructuring
+//Switches the values in the indexes of an array using destructuring
 // [main, secondary] = [secondary, main];
 
-// //Recieve 2 return values from the order function
+//Recieve 2 return values from the order function
 // const [starter, mainCourse] = restaurant.order(2, 0);
 
-// //Nested Destructuring
+//Nested Destructuring
 // const nested = [2, 4, [5, 6]];
 // // const [i, , j] = nested;
 // const [i, , [j, k]] = nested;
 // console.log(i, j, k);
 
-// //Default values
+//Default values
 // const [p = 1, q = 1, r = 1] = [8];
 // console.log(p, q, r);
+
+//Calling the function orderDelivery with object arguments
+//This defines all of the agurments passed in to the function
+// restaurant.orderDelivery({
+//   time: '22:38',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+//This defines only the address and starterIndex. The other arguments are defined in the defaults of the function.
+// restaurant.orderDelivery({
+//   address: 'Via del Sole, 21',
+//   starterIndex: 2,
+// });
+//Object Destructuring
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+//This assigns the object methods to different names for the stored variables
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+//Setting default values for object destructuring
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+//Mutating varibles in object destructuring
+// let a = 111;
+// let b = 119;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log(a, b);
+
+//Destructuring nested objects
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+//The SPREAD OPERATOR IS ... (three dots)
+//The Spread operator can work on all iterables
+//Iterables in JS are arrays, strings, maps and sets. NOT objects
+
+// Creating a new array out of anther array without spread operator
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+//Creating new array from an old array with the spread operator
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+//Can alsoget all of the elements from an array with the spread operator
+//This does not just log the elements but the values of the indecies as if being looped over
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+//Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+//Join 2 or more arrays
+// const wholeMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(wholeMenu);
+
+//Use with string
+// const str = 'Jonas';
+// const lettersFromStr = [...str, ' ', 'S.'];
+// console.log(lettersFromStr);
+// console.log(...str);
+
+//Use with functions (orderPasta function from above)
+// const ingredients = [
+//   prompt("Let's make pasta!\n Ingredient 1:"),
+//   prompt("Let's make pasta!\n Ingredient 2:"),
+//   prompt("Let's make pasta!\n Ingredient 3:"),
+// ];
+// console.log(ingredients);
+// restaurant.orderPasta(...ingredients);
+
+//Objects
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy);
+
+//Rest pattern is also ... (three dots) but does the oppisite of the spread operator
+//Spread because it is on the right hand side of the assignment operator
+// const arr = [1, 2, ...[3, 4]];
+
+//This is the rest syntax because it is on the left side of the assignment operator
+// const [a, b, ...other] = [1, 2, 3, 4, 5];
+// console.log(a, b, other);
+
+// const [pizza, risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+//Rest syntax with objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(sat, weekdays);
+
+//Rest with functions
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+//Logical Operators
+//Use any data type, return any data type and short-circuting
+
+//OR operand will short circut when the first operand is a truthy value
+//If the first operand is truthy, the other operand will not be looked at
+// console.log(3 || 'Jonas'); //Will come back as 3
+// console.log('' || 'Jonas'); //Will come back as Jonas
+// console.log(true || 0); //Will come back as true
+// console.log(undefined || null); //Will come back as null
+
+// console.log('------OR-----');
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+// //Same as
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+//AND operator short circuts when the first operand is a falsy value
+//If the first operand is falsey, the other operand will not be looked at
+// console.log('------AND-----');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushroom', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
+//Nullish Coalescing Operator
+//Works with Nullish Types: null and undefined (NOT 0 or "")
+// restaurant.numGuests = 0;
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// const correctGuests = restaurant.numGuests ?? 10;
+// console.log(correctGuests);
