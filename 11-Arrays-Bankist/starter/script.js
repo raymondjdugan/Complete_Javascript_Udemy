4a515351242528970e8daf2d9640a6ceebf39c53
RayDugan <raymond.j.dugan@gmail.com>
2022-01-01 17:46:04 -0600

    Finished Challenge 4. Added sort feature, loan feature and deleting account feature

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,9 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovments = function (movements) {
+const displayMovments = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -80,9 +83,9 @@ const displayMovments = function (movements) {
   });
 };
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (account) {
+  account.balance = account.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${account.balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -115,6 +118,18 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
+
+const updateUI = function (account) {
+  //Display movements
+  displayMovments(account.movements);
+
+  //Display balance
+  calcDisplayBalance(account);
+
+  //Display summary
+  calcDisplaySummary(account);
+};
+
 //Event Handlers
 let currentAccount;
 
@@ -136,18 +151,74 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
-    //Display movements
-    displayMovments(currentAccount.movements);
-
-    //Display balance
-    calcDisplayBalance(currentAccount.movements);
-
-    //Display summary
-    calcDisplaySummary(currentAccount);
+    updateUI(currentAccount);
   }
 });
 
 //Transfer Money
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const recieverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+  if (
+    amount > 0 &&
+    recieverAcc &&
+    currentAccount.balance >= amount &&
+    recieverAcc?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    recieverAcc.movements.push(amount);
+    updateUI(currentAccount);
+  }
+});
+
+//Loan feature
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+//Sort
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  displayMovments(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
+//Deleting accounts
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    //Finding the index of the account to be deleted from the accounts array
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    //Deleting the account
+    accounts.splice(index, 1);
+
+    //Hiding the UI
+    containerApp.style.opacity = 0;
+  }
+
+  //Clearing the fields
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -214,3 +285,105 @@ btnLogin.addEventListener('click', function (e) {
 
 // const account = accounts.find(acc => (acc.owner = 'Jessica Davis'));
 // console.log(account1);
+
+//findindex method
+//Returns the index of the condition and not the value
+
+//Some Method: if any of the elements satisfies the condition set array.some(param => condition)
+
+//Every Method - if every element satisfies the conditions set
+//array.every(mov => condition)
+
+//Seperate callback
+// const deposit = mov => mov > 0;
+//array.some(deposit);
+
+//Flat Method - No callback function
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+// console.log(arr.flat());
+//The () will determine how deep to flatten the array: Default is 1
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+// console.log(arr.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+
+//FlatMap Method - combines the map and the flat methods
+//Cannot change how deep the method goes. Only 1 level.
+// const overallBalance = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overallBalance);
+
+//Sorting Arrays - sort will mutate the original array
+//The sort method will also convert to string then sort
+
+//Strings
+// const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// console.log(owners.sort());
+
+//Numbers
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// console.log(movements);
+
+//Acending
+// console.log(
+//   movements.sort((a, b) => {
+//     if (a > b) return 1;
+//     if (a < b) return -1;
+//   })
+// );
+
+//Acending Refactored
+// movements.sort((a, b) => a - b);
+// console.log(movements);
+//Decending
+// console.log(
+//   movements.sort((a, b) => {
+//     if (a > b) return -1;
+//     if (a < b) return 1;
+//   })
+// );
+
+//Decending Refactored
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+
+//Progamatically create and fill arrays
+//array.fill()
+//Array.from(length: [#], () => condition)
+
+//Summary
+// Mutate the original array
+// Add to the original array: .push() and .unshift()
+// Remove from the original array: .pop(), .shift() and .splice()
+// Others: .reverse(), .sort() and .fill()
+
+// A New Array:
+// Computed from original: .map()
+// Filter using condition: filter()
+// Portion of the original: .slice()
+// Adding original to another: .concat()
+// Flattening the original: flat() and flatMap()
+
+// An Array Index
+// Based on value: .indexOf()
+// Based on test condition: .findIndex()
+
+// An Array Element
+//Based on test condition: .find()
+
+// Know if an Array includes
+// Based on value: .includes()
+// Based on test condition: .some() and .every()
+
+// A new string
+// Based on seperator string: .join()
+
+// To transform to value
+// Based on accumulator: .reduce()
+
+// To just loop the array
+// Based on callback: .forEach()
